d66451f5a54360187dbee688c0bbc8a586325625
berkansasmaz <berkansasmazz@gmail.com>
2019-10-21 22:03:32 +0300

    Initial commit

---
+++ CitySecuritySmart/Controllers/HakkimizdaController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CitySecuritySmart.Controllers
+{
+    public class HakkimizdaController : Controller
+    {
+        public IActionResult Ekip()
+        {
+            return View();
+        }
+    }
+}
---
+++ CitySecuritySmart/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using CitySecuritySmart.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CitySecuritySmart.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
